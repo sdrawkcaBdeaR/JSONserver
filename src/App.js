fa3ae5c73f5aa33c2ce9c7a56e4d07352089d80a
Nitro <cnitinrohit@gmail.com>
2023-08-09 23:23:58 +0530

    using json-server to make rest api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useState } from 'react'
+import bulma from 'bulma/css/bulma.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+    const [data,setData]=useState([])
+    const [name,setName]=useState("")
+    const [team,setTeam]=useState('')
+
+    const HandleClick=async ()=>{
+            await axios.post('http://localhost:3031/posts',{"name":name,"team":team})
+            .then(response => {
+                console.log('New post created:', response.data);
+              })
+              .catch(error => {
+                console.error('Error creating post:', error);})
+              
+
+            await axios.get('http://localhost:3031/posts')
+            .then(res=>setData(res.data))
+            .catch(err=>console.log(err))
+
+            setName('');
+            setTeam('')
+    }
+    const handleChangeName=(event)=>{
+        setName(event.target.value)
+    }
+    const handleChangeTeam=(event)=>{
+        setTeam(event.target.value)
+    }
+    const handleDelete=(objectId)=>async (event)=>{
+            await axios.delete(`http://localhost:3031/posts/${objectId}`)
+            .then(response => {
+                console.log('New post created:', response.data);
+              })
+              .catch(error => {
+                console.error('Error creating post:', error);})
+              
+
+            await axios.get('http://localhost:3031/posts')
+            .then(res=>setData(res.data))
+            .catch(err=>console.log(err))
+    }
+
+    return (
+        <>
+            <div style={{display:'flex', margin:"10px"}}>
+                <input className='input is-rounded' placeholder='Name' value={name} onChange={handleChangeName}/>
+                <input className='input is-rounded' placeholder='Club Name' value={team} onChange={handleChangeTeam}/>
+                <button onClick={HandleClick} className='button is-rounded'>
+                    add Detail
+                </button>
+            </div>
+            <div style={{margin:"10px",display:'flex',justifyContent:'space-around'}}>
+                <table className='table is-bordered'>
+                    <thead>
+                        <tr>
+                            <th>S.No.</th>
+                            <th>Name</th>
+                            <th>Club Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {data.map((details,index)=>{
+                                return (<tr>
+                                    <td onClick={handleDelete(details.id)} >{index}</td>
+                                    <td>{details.name}</td>
+                                    <td>{details.team}</td>
+                                </tr>)
+                            })}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
 }
 
 export default App;
